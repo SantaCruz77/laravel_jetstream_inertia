79a2e19d0f37e6a13538312829a1654d22e6830d
SantaCruz77 <steveyamada66@gmail.com>
2023-01-09 20:06:40 +0900

    first commit

---
+++ resources/js/Pages/Blog/Index.vue
@@ -0,0 +1,34 @@
+<script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
+defineProps({
+    blogs: Array,
+});
+</script>
+
+<template>
+    <AppLayout title="Blog">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Blog
+            </h2>
+        </template>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>タイトル</th>
+                            <th>コンテンツ</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="blog in blogs" :key="blog.id">
+                            <td class="border px-4 py-2">{{ blog.title }}</td>
+                            <td class="border px-4 py-2">{{ blog.content }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </AppLayout>
+</template>
